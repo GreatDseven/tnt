e84da8356bd5ce548fe2575e2c0f0837bf46bb2f
GreatDseven <582858152@qq.com>
2019-08-28 22:19:11 +0800

    282219

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
+import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Main from './components/Main/Main'
-import React, { Component } from 'react'
-class App extends Component {
-  state = {}
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Main />
-        </div>
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Main />
+      </div>
+    </Router>
+  )
 }
 export default App
